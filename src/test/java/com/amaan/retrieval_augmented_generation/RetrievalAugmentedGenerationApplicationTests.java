7d0b4ee9ddc55ce617bff00c7c44e333c79da335
Amaanali Parchigar <amaanaliparchigar0918@gmail.com>
2026-06-02 09:24:34 +0530

    first commit

---
+++ src/test/java/com/amaan/retrieval_augmented_generation/RetrievalAugmentedGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amaan.retrieval_augmented_generation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrievalAugmentedGenerationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
